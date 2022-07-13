04c00324e8a360a00a333e9b1f747a0fb7afbcd6
roooaaant <brew0@naver.com>
2022-07-14 02:53:24 +0900

    categories

---
+++ src/components/Categories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const categories = [
+    {
+        name : 'all',
+        text : '전체보기'
+    },
+    {
+        name : 'business',
+        text : '비즈니스'
+    },
+    {
+        name : 'entertainment',
+        text : '엔터테이먼트'
+    },
+    {
+        name : 'health',
+        text : '건강'
+    },
+    {
+        name : 'science',
+        text : '과학'
+    },
+    {
+        name : 'sports',
+        text : '스포츠'
+    }
+]
+
+
+export default test;
