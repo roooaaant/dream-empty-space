1665d530233dd8bb1913eb115c70c4b0c93ddd0e
roooaaant <brew0@naver.com>
2022-07-14 01:40:16 +0900

    News Viewer UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList'
+//import axios from 'axios';
 
+/*
 const App = () => {
   const [data, setData] = useState(null);
   const onClick = async () => {
     try {
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1'
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=d46e450eac8f4fefbcf58fc0d9cfdb6e',
       );
       setData(response.data);
     } catch (e) {
@@ -18,9 +20,13 @@ const App = () => {
       <div>
         <button onClick={onClick}>불러오기</button>
       </div>
-      {data && <textarea row={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+      {data && <textarea row={7} defaultValue={JSON.stringify(data, null, 2)} />}
     </div>
   );
 }
+ */
+const App = () => {
+  return <NewsList />
+};
 
 export default App;
